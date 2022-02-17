776e53cbb66b71c2ff8158c1e9aebc0d6ae8c558
micaam <mmicamenendez@gmail.com>
2022-02-17 08:15:41 -0300

    Ajustes de imagenes

--- js/main.js
+++ js/main.js
@@ -1,11 +1,5 @@
 window.onload = function () {
-    window.addEventListener('scroll', function (e) {
-        if (window.pageYOffset > 100) {
-            document.querySelector("header").classList.add('is-scrolling');
-        } else {
-            document.querySelector("header").classList.remove('is-scrolling');
-        }
-    });
+    
 
     const menu_btn = document.querySelector('.hamburger');
     const mobile_menu = document.querySelector('.mobile-nav');
